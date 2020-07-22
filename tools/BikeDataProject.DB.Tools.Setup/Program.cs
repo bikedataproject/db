bf416194256145615dbe704173548e9417c8d9bb
xivk <ben@itinero.tech>
2020-07-22 10:52:49 +0100

    Added a setup project to create db.

---
+++ tools/BikeDataProject.DB.Tools.Setup/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Tasks;
+using BDPDatabase;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
+
+namespace BikeDataProject.DB.Tools.Setup
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // read configuration.
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddUserSecrets<SetupTask>()
+                .Build();
+            
+            // setup serilog logging (from configuration).
+            var logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+            
+            // get database connection.
+            var connectionString = configuration["BikeDataProject:ConnectionString"];
+            
+            // setup DI.
+            var serviceProvider = new ServiceCollection()
+                .AddLogging()
+                .AddSingleton<SetupTask>()
+                .AddDbContext<BikeDataDbContext>(
+                    options => options.UseNpgsql(connectionString))
+                .BuildServiceProvider();
+
+            // add serilog logger to DI provider.
+            serviceProvider.GetRequiredService<ILoggerFactory>()
+                .AddSerilog();
+            
+            // do the actual work here
+            var task = serviceProvider.GetService<SetupTask>();
+            await task.Run();
+        }
+    }
+}
