fd0fbd1b7643fa61f511806c219f0509c9082874
Richez Antoine <richezatr@gmail.com>
2020-07-22 10:52:12 +0200

    Added migration

--- database-models-package/Migrations/20200722085039_AddedUserIsHistoryFetchedField.Designer.cs
+++ database-models-package/Migrations/20200722085039_AddedUserIsHistoryFetchedField.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using BDPDatabase;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace bikedataproject_database_models_package.Migrations
 {
     [DbContext(typeof(BikeDataDbContext))]
-    partial class BikeDataDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200722085039_AddedUserIsHistoryFetchedField")]
+    partial class AddedUserIsHistoryFetchedField
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
