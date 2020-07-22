63fab4e935524fdc5378bee071655a403585753d
xivk <ben@itinero.tech>
2020-07-22 09:41:31 +0100

    Added db migration project.

---
+++ tools/BikeDataProject.DB.Tools.MigrateOldDb/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BikeDataProject.DB.Tools.MigrateOldDb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
